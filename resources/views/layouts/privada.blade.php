7368ff2262f17ebb0d62f8cae4414ba4722ba07f
Mario Martínez Alfonso <mario.maralf@gmail.com>
2017-04-19 17:03:47 +0200

    corregido miniatura al colapsar barra lateral

--- resources/views/layouts/privada.blade.php
+++ resources/views/layouts/privada.blade.php
@@ -61,7 +61,7 @@ desired effect
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>Crs</b></span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg">Crisantemo</span>
     </a>
