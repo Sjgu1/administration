8ee781b7cd436a838f362234dee0c42a58881f35
Laailap <laila.pc@gmail.com>
2017-05-02 11:55:38 +0200

    siguiendo con los create

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,8 @@ Route::get('index/{field?}', 'RequisitosController@search');
 
 //Usuario
 Route::get('user/{id}/proyectosusers', 'InsideController@searchProyecto');
-//Route::post('proyecto/new', function(){ return view ('proyecto/{id}/proyectonew'); });
+Route::get('user/{id}/proyecto/new', function(){ return view('user.proyectonew'); });
+Route::post('user/{id}/proyecto/create', 'InsideController@createProyecto');
 Route::get('user/{id}/requisitosusers', 'InsideController@searchRequisito');
 Route::get('user/{id}/sprintsusers', 'InsideController@searchSprint');
 Route::group(['middleware'=>'auth'], function(){
