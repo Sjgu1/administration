a01c4c259d926cf7a8dba48deb8de91893279d5b
Laailap <laila.pc@gmail.com>
2017-05-17 12:33:31 +0200

    traducido registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
   @section('seleccion')
 <ul class="nav navbar-nav">
-  <li><a href="{{ url('/home') }}">Home</a></li>
-  <li ><a href="{{ url('/proyecto') }}">Proyecto</a></li>
-  <li><a href="{{ url('/contacto') }}">Contacto</a></li>
+  <li><a href="{{ url('/home') }}">@lang('messages.inicio')</a></li>
+  <li ><a href="{{ url('/proyecto') }}">@lang('messages.proyecto')</a></li>
+  <li><a href="{{ url('/contacto') }}">@lang('messages.contacto')</a></li>
 </ul>
 <body>
 @endsection
@@ -12,13 +12,13 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">@lang('messages.registro')</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
                         <!--UserName-->
                         <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <label for="username" class="col-md-4 control-label">User Name</label>
+                            <label for="username" class="col-md-4 control-label">@lang('messages.nombre de usuario')</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="text" class="form-control" name="username" value="{{ old('name') }}" required autofocus>
@@ -32,7 +32,7 @@
                         </div>
                         <!--Name-->
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">@lang('messages.nombre')</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
@@ -46,7 +46,7 @@
                         </div>
                         <!--Apellidos-->
                         <div class="form-group{{ $errors->has('apellidos') ? ' has-error' : '' }}">
-                            <label for="apellidos" class="col-md-4 control-label">Apellidos</label>
+                            <label for="apellidos" class="col-md-4 control-label">@lang('messages.apellidos')</label>
 
                             <div class="col-md-6">
                                 <input id="apellidos" type="text" class="form-control" name="apellidos" value="{{ old('apellidos') }}" required autofocus>
@@ -60,7 +60,7 @@
                         </div>
                         <!--E-Mail-->
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Email</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -74,7 +74,7 @@
                         </div>
                         <!--Password-->
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">@lang('messages.contrasenya')</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -88,7 +88,7 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 control-label">@lang('messages.confirmar contraseña')</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -98,7 +98,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                   @lang('messages.registrarse')
                                 </button>
                             </div>
                         </div>
