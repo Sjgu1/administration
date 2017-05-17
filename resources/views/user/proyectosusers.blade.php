02fbae6a8bddeeba8730d451cba92bc31b0ec463
Mario Martínez Alfonso <mario.maralf@gmail.com>
2017-05-17 21:45:00 +0200

    ProyectoUsers icono centrado, primera letra del proyecto en proceso

--- resources/views/user/proyectosusers.blade.php
+++ resources/views/user/proyectosusers.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.privada')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
 
@@ -10,20 +11,47 @@ $(document).ready(function() {
     })
 
 })
+
 </script>
+
+<style>
+	.centrate{
+		padding: 25%;
+	}
+</style>
+
 	@section('content')
-	<h1> @lang('messages.proyectos')</h1>
-		@foreach($proyectosusers as $proyectouser)
-				<div class="col-md-3 col-sm-6 col-xs-12">
-					<div class="info-box bg-aqua">
-						<span class="info-box-icon"><i class="fa fa-calendar"></i></span>
-						<div class="info-box-content">
-							<span class="info-box-text">{{ $proyectouser->proyecto->nombre }}</span>
-							
-						</div><!-- /.info-box-content -->
-					</div><!-- /.info-box -->
-				</div>
-				@endforeach
+	<section class="content-header">
+        <h1> @lang('messages.proyectos')</h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#">UI</a></li>
+            <li class="active">Timeline</li>
+        </ol>
+    </section>
+	<div class="row">
+		<section class="content connectedSortable">
+			@foreach($proyectosusers as $proyectouser)
+					<div class="col-md-3 col-sm-6 col-xs-12">
+						<div class="info-box bg-aqua">
+							<span class="info-box-icon"><div class="profileImage"></div></span>
+							<div class="info-box-content">
+								<span class="info-box-text firstName">{{ $proyectouser->proyecto->nombre }}</span>
+								
+							</div><!-- /.info-box-content -->
+						</div><!-- /.info-box -->
+					</div>
+					<script>
+							$(document).ready(function(){
+						var firstName = $('.firstName').text();
+						var intials = $('.firstName').text().charAt(0);
+						var profileImage = $('.profileImage').text(intials);
+						});
+					</script>
+					@endforeach
+		</section>
+	</div>
+	
 
 	<!--<table id="proyectosusers" class="table table-striped table-bordered display" cellspacing="0" width="100%">
     	<thead>
