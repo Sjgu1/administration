a81cd91478bd6eecb09bd421ea20f62ae7153f9e
Mario Martínez Alfonso <mario.maralf@gmail.com>
2017-05-21 12:15:05 +0200

    Cambiado onclick del proyecto al cuadro entero

--- resources/views/user/proyectosusers.blade.php
+++ resources/views/user/proyectosusers.blade.php
@@ -50,8 +50,8 @@ $(document).ready(function() {
 		<section class="content connectedSortable">
 			@foreach($proyectosusers as $proyectouser)
 				<div class="col-md-3 col-sm-6 col-xs-12">
-					<div class="info-box bg-aqua">
-						<span class="info-box-icon btn" onclick="sesionProyecto({{$proyectouser->proyecto}})"><div class="profileImage{{$proyectouser->proyecto->id}}"></div></span>
+					<div class="info-box bg-aqua" onclick="sesionProyecto({{$proyectouser->proyecto}})">
+						<span class="info-box-icon btn" ><div class="profileImage{{$proyectouser->proyecto->id}}"></div></span>
 						<div class="info-box-content">
 							<span class="info-box-text firstName{{$proyectouser->proyecto->id}}">{{ $proyectouser->proyecto->nombre }}</span>
 							
