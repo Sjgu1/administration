f6b94e10851c12af9138ac5a94fd8c676c4e5c25
Sergio Julio Garcia Urdiales <sjgu1@alu.ua.es>
2017-05-22 20:32:26 +0200

    Me deje un endesection extra, sorry my bad

--- resources/views/user/proyectosusers.blade.php
+++ resources/views/user/proyectosusers.blade.php
@@ -18,7 +18,6 @@
     @endif
 @endif
 @endsection
-@endsection
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
